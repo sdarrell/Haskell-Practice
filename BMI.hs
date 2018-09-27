75b7dea94e8a32b608d63adb30b94bc7d037dd78
sdarrell <42746528+sdarrell@users.noreply.github.com>
2018-09-27 21:29:40 +0300

    Update BMI.hs
    skinny, normal, fat.

--- BMI.hs
+++ BMI.hs
@@ -1,7 +1,11 @@
 bmi :: Float -> Float -> String
 bmi weight height
- | bmi' <= 18.5 = "Underweight"
- | bmi' <= 25.0 = "Normal"
- | bmi' <= 30 = "Overweight"
- | otherwise = "Obese"
+ | bmi' <= skinny = "Underweight"
+ | bmi' <= normal = "Normal"
+ | bmi' <= fat    = "Overweight"
+ | otherwise      = "Obese"
  where bmi' = weight / (height^2)
+       skinny = 18.5
+       normal = 25.0
+       fat    = 30.0
+       
