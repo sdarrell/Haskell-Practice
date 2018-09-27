d223f716ecf4675e6630abc464cac1fef79115c7
sdarrell <42746528+sdarrell@users.noreply.github.com>
2018-09-27 17:38:57 +0300

    Update BMI.hs
    instead of BMI > 30 = "Obese"

--- BMI.hs
+++ BMI.hs
@@ -3,5 +3,5 @@ bmi weight height
  | bmi' <= 18.5 = "Underweight"
  | bmi' <= 25.0 = "Normal"
  | bmi' <= 30 = "Overweight"
- | bmi' > 30 = "Obese"
+ | otherwise = "Obese"
  where bmi' = weight / (height^2)
